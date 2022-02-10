da0a978e8d0858a0ccf7fa876bf1b786ce345db3
Domen <domen.lisjak@inova.si>
2022-02-10 13:56:43 +0100

    remove blur

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -8,7 +8,6 @@ const Header = ({ image, alt, objectPosition = 'center', title, subtitle, button
         alt={alt}
         objectFit="cover"
         objectPosition={objectPosition}
-        placeholder="blur"
         priority={true}
       />
     </div>
