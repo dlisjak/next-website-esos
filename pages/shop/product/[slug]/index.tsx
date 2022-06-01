e1ca2080d3652feac6525edc9d05be8397e4f3ef
Domen <domen.lisjak@inova.si>
2022-06-01 20:17:01 +0200

    add export

--- pages/shop/product/[slug]/index.tsx
+++ pages/shop/product/[slug]/index.tsx
@@ -1,55 +1,53 @@
-// import Image from 'next/image';
-// import React from 'react';
+import Image from 'next/image';
+import React from 'react';
 
-// const Product = ({ product }) => {
-//   const { name, slug, optionGroups, description, variants, featuredAsset } = product;
-//   console.log(product);
-//   return (
-//     <div className="relative bg-[#EfEfEf] h-full text-black py-12 px-4">
-//       <div className="container m-auto flex pt-20">
-//         <div className="relative w-80 mr-8">
-//           <Image src={featuredAsset.source} layout="responsive" width={450} height={600} />
-//         </div>
-//         <div className="flex flex-col">
-//           <h1>{name}</h1>
-//           <div className="flex flex-col">
-//             {optionGroups.map((optionGroup) => (
-//               <div className="flex" key={optionGroup.code}>
-//                 {optionGroup.name}
-//               </div>
-//             ))}
-//           </div>
-//           <div dangerouslySetInnerHTML={{ __html: description }} />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
+const Product = ({ product }) => {
+  const { name, slug, optionGroups, description, variants, featuredAsset } = product;
+  console.log(product);
+  return (
+    <div className="relative bg-[#EfEfEf] h-full text-black py-12 px-4">
+      <div className="container m-auto flex pt-20">
+        <div className="relative w-80 mr-8">
+          <Image src={featuredAsset.source} layout="responsive" width={450} height={600} />
+        </div>
+        <div className="flex flex-col">
+          <h1>{name}</h1>
+          <div className="flex flex-col">
+            {optionGroups.map((optionGroup) => (
+              <div className="flex" key={optionGroup.code}>
+                {optionGroup.name}
+              </div>
+            ))}
+          </div>
+          <div dangerouslySetInnerHTML={{ __html: description }} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-// export const getStaticPaths = async () => {
-//   const client = (await import('../../../../lib/apollo')).default;
-//   const query = (await import('../../../../gql/product')).ALL_PRODUCTS_QUERY;
+export const getStaticPaths = async () => {
+  // const client = (await import('../../../../lib/apollo')).default;
+  // const query = (await import('../../../../gql/product')).ALL_PRODUCTS_QUERY;
 
-//   const { data } = await client.query({ query });
+  // const { data } = await client.query({ query });
 
-//   const paths = data.products.items.map((item) => ({ params: { slug: item.slug } }));
+  // const paths = data.products.items.map((item) => ({ params: { slug: item.slug } }));
 
-//   return { paths, fallback: false };
-// };
+  return { fallback: false };
+};
 
-// export const getStaticProps = async ({ params }) => {
-//   const client = (await import('../../../../lib/apollo')).default;
-//   const query = (await import('../../../../gql/product')).SINGLE_PRODUCT_QUERY;
+export const getStaticProps = async ({ params }) => {
+  // const client = (await import('../../../../lib/apollo')).default;
+  // const query = (await import('../../../../gql/product')).SINGLE_PRODUCT_QUERY;
 
-//   const { data } = await client.query({ query, variables: { slug: params.slug } });
+  // const { data } = await client.query({ query, variables: { slug: params.slug } });
 
-//   return {
-//     props: {
-//       product: data.product,
-//     },
-//   };
-// };
+  return {
+    props: {
+      product: [],
+    },
+  };
+};
 
-// export default Product;
-
-export {};
+export default Product;
