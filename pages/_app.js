f8944fa8534fedc35eaa71419a1b5aa59470ab1e
Domen <domen.lisjak@inova.si>
2022-09-15 07:02:43 +0200

    add landing page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import { SwitchTransition, CSSTransition } from 'react-transition-group';
 
 import Footer from '../components/Footer';
-import NavBar from '../components/NavbBar';
+import NavBar from '../components/NavBar';
 import NavBarNew from '../components/NavbarNew';
 
 import '../styles/main.css';
